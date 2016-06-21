d91bd59fb82fdd171835b43347a3a61fdf7d75f8
ishniratech <rajesh@ishnira.com>
2016-06-21 17:24:31 +0530

    Initial commit

---
+++ app/src/main/java/ishnira/tech/com/Constants.java
@@ -0,0 +1,7 @@
+package ishnira.tech.com;
+
+public class Constants {
+    public static final String  DEFAULT_CITY = "Bangalore";
+    public static final String  DEFAULT_LAT = "12.9716";
+    public static final String  DEFAULT_LON = "77.5946";
+}
